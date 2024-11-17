34cee7af531e866829ebc0c2400bc43c1055b4d8
Tim <tdmorton22@gmail.com>
2024-11-17 15:53:27 -0500

    updated button click events

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,7 +21,52 @@ namespace CalculatorGUI
             InitializeComponent();
         }
 
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void Btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn0_Click(object sender, RoutedEventArgs e)
         {
 
         }
